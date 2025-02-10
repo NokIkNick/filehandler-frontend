1bafca2cb6974ebb2c69e732816655fe0ad34e59
Nicklas Waldemar Seier Winther <nsw@convergens.lan>
2025-02-10 14:33:29 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent],
+  template: `
+    <app-header></app-header>
+    <app-dashboard></app-dashboard>
+  `,
+  styles: `
+    
+  `
 })
 export class AppComponent {
-  title = 'filehandler-frontend';
 }
